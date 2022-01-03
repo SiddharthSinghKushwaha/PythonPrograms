71b3539a863a04ce8eeede775e9b04331316b1ad
SiddharthSinghKushwaha <siddharthsk211@gmail.com>
2022-01-03 18:00:16 +0530

    largest between two number added

---
+++ LargestOfTwoNumber.py
@@ -0,0 +1,9 @@
+# Take 2 numbers as input and print the largest number without using function
+
+num1 = int(input('Enter 1st number: '))
+num2 = int(input('Enter 2nd number: '))
+
+if num1 > num2:
+    print(num1, ' is the largest number')
+else:
+    print(num2, ' is the largest number')
