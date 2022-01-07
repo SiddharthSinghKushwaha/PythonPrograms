3ca74345509c2078c7f234ee4131f96f6af7ecee
SiddharthSinghKushwaha <siddharthsk211@gmail.com>
2022-01-07 16:32:24 +0530

    swapping number without extra variable

--- SwappingNumber.py
+++ SwappingNumber.py
@@ -4,10 +4,24 @@
 
 a = int(input('Enter first number: '))
 b = int(input('Enter second number: '))
+
+# done using an extra variable 'temp' METHOD 1
+print("METHOD 1")
 print('Value before swapping ', a, ' ', b)
 
 temp = a
 a = b
 b = temp
+
+print("Value after swapping", a, ' ', b)
+
+# using no extra variable METHOD 2
+a, b = 40, 50
+print("\nMethod 2")
+print('Value before swapping ', a, ' ', b)
+
+a = a + b
+b = a - b
+a = a - b
 
 print("Value after swapping", a, ' ', b)
