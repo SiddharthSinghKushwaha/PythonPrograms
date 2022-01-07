fd1d84be46e956b6b37b25bc74925878144b680b
SiddharthSinghKushwaha <siddharthsk211@gmail.com>
2022-01-07 16:58:28 +0530

    largest no. without using function

---
+++ MaxOfThreeNumber.py
@@ -0,0 +1,15 @@
+# Write a program to find the largest number among three number without using in-built function.
+a = int(input('Enter 1st no.: '))
+b = int(input('Enter 2nd no.: '))
+c = int(input('Enter 3rd no.: '))
+
+if a > b:
+    if a > c:
+        print(a, ' is the largest number')
+    else:
+        print(c, ' is the largest number')
+else:
+    if b > c:
+        print(b, ' is the largest number')
+    else:
+        print(c, ' is the largest number')
